e6fd9050428dc7e4dfc8f62c9e259f8d6c76f14b
oupson <oupson1er@gmail.com>
2025-01-03 15:01:54 +0100

    Remove fake http client.
    # Conflicts:
    #	src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
-// Importation des modules spécifiques
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { PokemonModule } from './pokemon/pokemon.module'; // Module Pokémon
-import { LoginComponent } from './login/login.component'; // Composant pour la connexion
+import {PokemonModule} from "./pokemon/pokemon.module";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDataService} from "./in-memory-data.service";
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-// @ts-ignore
+
 @NgModule({
   declarations: [
-    // Déclaration des composants utilisés dans ce module
     AppComponent,
     PageNotFoundComponent,
     LoginComponent,
+    SignupComponent
   ],
   imports: [
-    // Importation des modules nécessaires
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    PokemonModule, // Importation du module Pokémon
-    AppRoutingModule, // Configuration des routes de l'application
+    PokemonModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent], // Composant racine
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
