6e78101297532a4411f68c53d816884a895b952a
oupson <oupson1er@gmail.com>
2025-01-03 15:03:21 +0100

    Add a login button to the header.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component , OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent{}
+export class AppComponent{
+  constructor(public authService : AuthService) {
+  }
+}
